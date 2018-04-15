43786a2b21f4a712818227711085a4177c18b1c6
Larry Bohn <larrybohn@users.noreply.github.com>
2018-04-15 15:45:51 +0300

    Added my fork of Telebot and mounted telebot listener into the Koa app.

--- src/web/index.js
+++ src/web/index.js
@@ -11,38 +11,52 @@ import webpackConfig from '../../webpack.development';
 import koaWebpackDevMiddleware from 'koa-webpack-dev-middleware';
 import koaWebpackHotMiddleware from 'koa-webpack-hot-middleware';
 
-const app = new Koa();
-const router = new Router();
+import startTelegramBotWithWebhook from '../bot/index.js';
 
-router
-    .use('/api', api.routes(), api.allowedMethods())
-    .use('/auth', auth.routes(), auth.allowedMethods());
+async function initKoaApp() {
+    const app = new Koa();
+    const router = new Router();
 
-if (!config.debug) {
     router
-        .get('/assets/*', async (ctx) => {
-            await send(ctx, ctx.path, { root: __dirname + '/public'});
-        })
-        .get('/*', async (ctx) => {
-            await send(ctx, 'public/index.html', {root: __dirname });
+        .use('/api', api.routes(), api.allowedMethods())
+        .use('/auth', auth.routes(), auth.allowedMethods());
+
+    if (config.webhookUrl) {
+        await startTelegramBotWithWebhook().then(listener => {
+            router.post('/telegram/:token', async (ctx) => {
+                listener(ctx.req, ctx.res);
+            })
         });
-}else{
-    const compiler = webpack(webpackConfig);
-    const koaWebpackDevMiddlewareInstance = koaWebpackDevMiddleware(compiler, {publicPath: '/'});
-    router
-        .get('/*', (ctx, next) => {
-            if (['/reminders', '/timezone', '/keyboard'].indexOf(ctx.request.path) !== -1) {
-                ctx.request.path = '/';
-            }
-            return koaWebpackDevMiddlewareInstance(ctx, next);
-        });
+    }
+
+    if (!config.debug) {
+        router
+            .get('/assets/*', async (ctx) => {
+                await send(ctx, ctx.path, { root: __dirname + '/public' });
+            })
+            .get('/*', async (ctx) => {
+                await send(ctx, 'public/index.html', { root: __dirname });
+            });
+    } else {
+        const compiler = webpack(webpackConfig);
+        const koaWebpackDevMiddlewareInstance = koaWebpackDevMiddleware(compiler, { publicPath: '/' });
+        router
+            .get('/*', (ctx, next) => {
+                if (['/reminders', '/timezone', '/keyboard'].indexOf(ctx.request.path) !== -1) {
+                    ctx.request.path = '/';
+                }
+                return koaWebpackDevMiddlewareInstance(ctx, next);
+            });
+        app
+            .use(koaWebpackHotMiddleware(compiler, { path: '/__webpack_hmr' }));
+    }
+
     app
-        .use(koaWebpackHotMiddleware(compiler, {path: '/__webpack_hmr'}));
-}
-
-
-app
     .use(router.routes())
     .use(router.allowedMethods());
 
-app.listen(process.env.PORT || 3200);
+    app.listen(process.env.PORT || 3200);
+
+}
+
+initKoaApp();
