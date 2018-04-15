d6d89f30aeaee39c913f43871646c15d685fd58b
Larry Bohn <larrybohn@users.noreply.github.com>
2018-04-16 00:52:05 +0300

    Added logic to create databases to deploy-database.js.

--- deploy-database.js
+++ deploy-database.js
@@ -1,6 +1,8 @@
 import childProcess from 'child_process'
 import bluebird from 'bluebird';
 import fs from 'fs';
+import Nano from 'nano';
+
 bluebird.promisifyAll(childProcess);
 
 const url = process.argv[2]
@@ -16,6 +18,16 @@ const designDocuments = [
     }
 ];
 
+const nano = Nano(url);
+bluebird.promisifyAll(nano.db);
+bluebird.all(['auth-tokens', 'reminders', 'user-settings'].map(dbName => {
+    return nano.db.getAsync(dbName).catch(() => {
+        console.log(`creating database ${dbName}`)
+        return nano.db.createAsync(dbName);
+    });
+}));
+
+console.log('Starting deployment');
 bluebird.all(designDocuments.map(d => {
     let deployCmd = `node ./couchmigrate/app.js --dd ${d.file} --db ${d.db}`;
     if (url) {
